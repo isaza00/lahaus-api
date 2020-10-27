ef57c62a32550bf9571ccf47ac8d611534b1d169
Norman <isaza00@hotmail.com>
2020-10-27 11:39:34 -0500

    adding location column to photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,5 +3,6 @@ class Photo < ApplicationRecord
   belongs_to :property
   validates :property_id, presence: true
   validates :url, presence: true
+  validates :location, length: { maximum: 255 }, allow_nil: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_123904) do
+ActiveRecord::Schema.define(version: 2020_10_27_163321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_10_23_123904) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "accepted_foc"
     t.boolean "accepted_lum"
+    t.string "location"
     t.index ["property_id"], name: "index_photos_on_property_id"
   end
 
